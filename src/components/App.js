c91d00aa4f6b3d20fdd39fed8412318100b242a4
Gjorgjioska <marina.gorgioska@gmail.com>
2022-02-27 12:55:11 +0100

    Building new BlogApp

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostList from "./PostList";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <h1>PostList</h1>
+    </div>
+  );
+};
+
+export default App;
